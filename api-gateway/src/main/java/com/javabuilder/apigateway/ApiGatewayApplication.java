3956b08b0fb6d9c6f848d0286c4a51fe568090b2
VoPhuocViet <viet62716@gmail.com>
2026-05-15 01:18:17 +0700

    service discovery

--- api-gateway/src/main/java/com/javabuilder/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/javabuilder/apigateway/ApiGatewayApplication.java
@@ -1,4 +1,4 @@
-package com.javabuilder.api_gateway;
+package com.javabuilder.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
