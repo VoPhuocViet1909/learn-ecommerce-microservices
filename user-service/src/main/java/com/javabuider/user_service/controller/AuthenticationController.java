88bfe9af48c48693c8a04a7c02f80a9a7bb6a57a
VoPhuocViet <viet62716@gmail.com>
2026-05-14 15:24:09 +0700

    Refresh Token with HttpOnly Cookie

--- user-service/src/main/java/com/javabuider/user_service/controller/AuthenticationController.java
+++ user-service/src/main/java/com/javabuider/user_service/controller/AuthenticationController.java
@@ -4,7 +4,8 @@ import com.javabuider.user_service.dto.request.LoginRequest;
 import com.javabuider.user_service.dto.response.ApiResponse;
 import com.javabuider.user_service.dto.response.LoginResponse;
 import com.javabuider.user_service.service.AuthenticationService;
-
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -20,9 +21,24 @@ public class AuthenticationController {
 
     private final AuthenticationService authenticationService;
 
-    @PostMapping( "/login")
-    ApiResponse<LoginResponse> login(@RequestBody @Valid LoginRequest request) {
+    @PostMapping("/login")
+    ApiResponse<LoginResponse> login(@RequestBody @Valid LoginRequest request, HttpServletResponse response) {
         var data = authenticationService.login(request);
+        
+        Cookie cookie = new Cookie("refresh_token", data.refreshToken());
+        cookie.setHttpOnly(true); // Prevents JavaScript from accessing the cookie (XSS protection)
+        cookie.setSecure(false); // Change to true in production
+        cookie.setPath("/"); // Cookie is accessible across all paths in the app
+        cookie.setMaxAge(14 * 24 * 60 * 60); // Cookie expiry: 14 days — matches refresh token TTL
+        response.addCookie(cookie);
+
+            // Tạo response mới không chứa refresh token
+        LoginResponse responseData = LoginResponse.builder()
+        .accessToken(data.accessToken())
+        .refreshToken(null) // Không trả về refresh token
+        .roles(data.roles())
+        .build();
+        
         return ApiResponse.<LoginResponse>builder()
                 .code(HttpStatus.OK.value())
                 .message("Login successful")
