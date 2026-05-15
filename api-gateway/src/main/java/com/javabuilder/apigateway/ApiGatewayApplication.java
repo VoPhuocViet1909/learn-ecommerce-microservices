da94e2e3a3eb6ba59e26928daeed9cf568d8eff5
VoPhuocViet <viet62716@gmail.com>
2026-05-15 12:48:45 +0700

    Verify Eureka Dashboard (Eureka Server, User Service, API Gateway)

--- api-gateway/src/main/java/com/javabuilder/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/javabuilder/apigateway/ApiGatewayApplication.java
@@ -2,8 +2,10 @@ package com.javabuilder.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient // Thêm dòng này
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
